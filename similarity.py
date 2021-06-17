63139111dfe61463bdc64b6c7dc7dc04618a13f8
Emeline Caruana <49732269+emeline-caruana@users.noreply.github.com>
2021-06-17 22:16:14 +0200

    Update similarity.py

--- similarity.py
+++ similarity.py
@@ -1,18 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+## File similarity.py
+
 import numpy as np
-import spacy
-from spacy import stats
+import scipy
+from scipy import stats, spatial
 from operator import itemgetter, attrgetter
 
-from data import *
-from retrofitting import *
+import sklearn
+from sklearn.metrics.pairwise import cosine_similarity
 
 def indice(liste_sim, autre_liste):
     ## Pour chaque élement de la liste de similarité, on met son indice+1 (= son rang) dans la liste de rang
     rang = []
     liste_tri = sorted(autre_liste)
     for i in range(len(liste_sim)) :
-        print(i)
-        print(liste_sim)
         if liste_sim[i] in liste_tri:
             indice = 1 + liste_tri.index(liste_sim[i])
             rang.append(indice)
@@ -51,23 +54,30 @@ def corr_spearman(embed_dict, simil_dict):
             for j in range(len(simil_dict[paires_mots[i][0]])):
                 if simil_dict[paires_mots[i][0]][j][0] == paires_mots[i][1]:
                     score_humain.append(simil_dict[paires_mots[i][0]][j][1])
-            mot1, mot2 = paires_mots[i][0], paires_mots[i][1]
-            embd1 = embed_dict[mot1]
-            embd2 = embed_dict[mot2]
+                    mot1, mot2 = paires_mots[i][0], paires_mots[i][1]
 
-            ## similarité cosinus
-            cos_score.append(1 - spatial.distance.cosine(embd1, embd2))
+                    ## similarité cosinus
+                    cos_score.append(1 - spatial.distance.cosine(embed_dict[mot1], embed_dict[mot2]))
 
-        ## On calcule le rang de chaque élément de la liste tout en triant le rang des éléments.
-        ## Puisque le coefficient de Spearman calcule le coefficient de corrélation sur les valeurs de rang des données
-        cos_rang = indice(cos_score, cos_score)
-        humain_rang = indice(score_humain, score_humain)
+    ## On calcule le rang de chaque élément de la liste tout en triant le rang des éléments.
+    ## Puisque le coefficient de Spearman calcule le coefficient de corrélation sur les valeurs de rang des données
+    cos_rang = indice(cos_score, cos_score)
+    humain_rang = indice(score_humain, score_humain)
 
-        ## corrélation de Spearman
-        spearman = np.corrcoef(humain_rang,cos_rang)
-        #spearman = spcipy.stats.spearmanr(humain_rang, cos_rang)
+    ## corrélation de Spearman
+    #spearman = np.corrcoef(humain_rang,cos_rang)
+    spearman = scipy.stats.spearmanr(humain_rang, cos_rang,nan_policy='omit')
 
-        return(spearman)
+    return spearman
 
   
-#print(corr_spearman(embeddings_dict,similarity_dict))
+sp = corr_spearman(embeddings_dict,similarity_dict)
+print("Corrélation de Spearman :",sp)
+
+def cosine_similarity(embed_dict,simil_dict,word_pairs):
+  new_simil_dict = {}
+  cos_list = []
+  for word1,word2 in word_pairs:
+    if (word1 in embed_dict.keys()) and (word2 in embed_dict.keys()):
+      cos = cosine_similarity(embed_dict[word1],embed_dict[word2])
+    cos_list.append(cos)
